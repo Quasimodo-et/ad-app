95d72b93fa8e762f85f5ff337b587ae646cfd3a2
Quasimodo-et <342835609@qq.com>
2026-08-22 03:25:12 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
 // 作用：网页被"安装"到手机/电脑桌面后，即使没有网络也能打开
 // ============================================================
 
-const CACHE_NAME = "ad-tracker-v4";
+const CACHE_NAME = "ad-tracker-v6";
 const APP_SHELL = [
   "./",
   "./index.html",
